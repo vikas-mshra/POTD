cedae5d51940efd656036dc02a28628a7ca604e0
Vikas Mishra <vikas.mishra0796@gmail.com>
2026-02-01 09:23:15 -0800

    Add DFS-based solution for Course Schedule II problem, implementing topological sort to determine course order based on prerequisites. Includes cycle detection and adjacency list construction.

--- Graphs/TopologicalSort/Course Schedule II.java
+++ Graphs/TopologicalSort/Course Schedule II.java
@@ -54,3 +54,57 @@ class Solution {
         return arr;
     }
 }
+
+/**
+    Approach: Topological sort using DFS
+    Time(n): O(E) + O(V + E)
+    Space(n): O(V + E)
+*/
+/**
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        // 1: build adjL
+        Map<Integer, List<Integer>> adjL = new HashMap<>();
+        for (int[] p : prerequisites) {
+            adjL.computeIfAbsent(p[1], k -> new ArrayList<>()).add(p[0]);
+        }
+
+        // 2: Iterate all courses
+        boolean[] isVisited = new boolean[numCourses];
+        boolean[] inRecursion = new boolean[numCourses];
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (!isVisited[i] && hasCycle(i, isVisited, inRecursion, stack, adjL)) {
+                return new int[0];
+            }
+        }
+
+        // 3: building the response
+        int[] arr = new int[numCourses];
+        int index = 0;
+        while (!stack.isEmpty())
+            arr[index++] = stack.pop();
+
+        return arr;
+    }
+
+    private boolean hasCycle(int u, boolean[] isVisited, boolean[] inRecursion, Stack<Integer> stack,
+            Map<Integer, List<Integer>> adjL) {
+        isVisited[u] = true;
+        inRecursion[u] = true;
+        for (int v : adjL.getOrDefault(u, Collections.emptyList())) {
+            if (inRecursion[v])
+                return true;
+
+            if (isVisited[v])
+                continue;
+
+            if (hasCycle(v, isVisited, inRecursion, stack, adjL))
+                return true;
+        }
+        inRecursion[u] = false;
+        stack.push(u);
+        return false;
+    }
+}
+*/
